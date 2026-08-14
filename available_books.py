9cee8ef7c627bca62d441ea133e56a6bb2d2eb04
sakshinikam0507 <sakshinikam.tech@gmail.com>
2026-08-14 15:07:19 +0530

    Delete available_books.py

--- available_books.py
+++
@@ -1,4 +0,0 @@
-available = {"Python Basics", "Java Programming", "Data Structures", "DBMS"}
-requested = {"Python Basics", "DBMS", "Operating Systems"}
-
-print("Available requested books:", available & requested)
