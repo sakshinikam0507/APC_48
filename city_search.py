b57f1942ed92e5f1fb0b70f7ed20f282556dec41
sakshinikam0507 <sakshinikam.tech@gmail.com>
2026-08-14 15:08:36 +0530

    Delete city_search.py

--- city_search.py
+++
@@ -1,8 +0,0 @@
-cities = ["Pune", "Mumbai", "Kolhapur", "Nashik", "Nagpur"]
-
-city = input("Enter city name: ")
-
-if city in cities:
-    print("City exists in the list")
-else:
-    print("City not found")
