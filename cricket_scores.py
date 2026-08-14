682b6c2bf660862d51304eff7d672979bbd6dba4
sakshinikam0507 <sakshinikam.tech@gmail.com>
2026-08-14 15:10:54 +0530

    Delete cricket_scores.py

--- cricket_scores.py
+++
@@ -1,6 +0,0 @@
-scores = (45, 102, 67, 12, 89, 134, 56, 78, 110, 39)
-
-print("Total runs:", sum(scores))
-print("Highest score:", max(scores))
-print("Lowest score:", min(scores))
-print("Average score:", sum(scores) / len(scores))
