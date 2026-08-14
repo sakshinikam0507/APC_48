690518f40a2434e7664a5648df35d8071c70e82b
sakshinikam0507 <sakshinikam.tech@gmail.com>
2026-08-14 15:29:36 +0530

    Delete student_search.py

--- student_search.py
+++
@@ -1,8 +0,0 @@
-students = {"Amit", "Riya", "Neha", "Rahul", "Priya"}
-
-name = input("Enter student name: ")
-
-if name in students:
-    print("Student exists")
-else:
-    print("Student not found")
