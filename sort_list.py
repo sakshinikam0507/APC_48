ab240afc9fdb6e5e437c0a02ed6eb6000c86d33a
sakshinikam0507 <sakshinikam.tech@gmail.com>
2026-08-14 15:27:23 +0530

    Delete sort_list.py

--- sort_list.py
+++
@@ -1,7 +0,0 @@
-numbers = []
-
-for i in range(10):
-    numbers.append(int(input("Enter a number: ")))
-
-print("Ascending order:", sorted(numbers))
-print("Descending order:", sorted(numbers, reverse=True))
