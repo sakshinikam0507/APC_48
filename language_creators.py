7822e4d54c45cae2eafdf327491e1831e5be3874
sakshinikam0507 <sakshinikam.tech@gmail.com>
2026-08-14 15:19:41 +0530

    Delete language_creators.py

--- language_creators.py
+++
@@ -1,9 +0,0 @@
-languages = {
-    "Python": "Guido van Rossum",
-    "Java": "James Gosling",
-    "C": "Dennis Ritchie",
-    "JavaScript": "Brendan Eich"
-}
-
-for language, creator in languages.items():
-    print(language, ":", creator)
