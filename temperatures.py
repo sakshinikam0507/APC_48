f719f9db0f9dc084a2687a9c7beebf8ea0dad72d
sakshinikam0507 <sakshinikam.tech@gmail.com>
2026-08-14 15:30:18 +0530

    Delete temperatures.py

--- temperatures.py
+++
@@ -1,5 +0,0 @@
-temperatures = (28, 31, 30, 33, 29, 35, 32)
-
-print("Maximum temperature:", max(temperatures))
-print("Minimum temperature:", min(temperatures))
-print("Average temperature:", sum(temperatures) / len(temperatures))
