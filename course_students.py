68f3d3abcb543f9b08d4b38c613c483013903be0
sakshinikam0507 <sakshinikam.tech@gmail.com>
2026-08-14 15:10:18 +0530

    Delete course_students.py

--- course_students.py
+++
@@ -1,5 +0,0 @@
-python_students = {"Amit", "Riya", "Neha", "Rahul"}
-java_students = {"Riya", "Rahul", "Priya", "Soham"}
-
-print("Both courses:", python_students & java_students)
-print("Only one course:", python_students ^ java_students)
